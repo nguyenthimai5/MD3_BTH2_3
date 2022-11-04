53fa7d6fa3032d52cd3edb681a1dfe20e2386f23
nguyenthimai5 <mikhailthv@gmail.com>
2022-11-04 09:20:25 +0700

    MD3_BTH2_3

--- BTH2_3.sql
+++ BTH2_3.sql
@@ -6,7 +6,10 @@ CREATE TABLE Class
     ClassName VARCHAR(60) NOT NULL,
     StartDate DATETIME    NOT NULL,
     Statuss    BIT
-);
+); 
+insert into class
+values(1,'LOP 12A7','2019-09-02',1),
+(2,'LOP 12A1','2018-05-12',1);
 CREATE TABLE Student
 (
     StudentId   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -17,6 +20,10 @@ CREATE TABLE Student
     ClassId     INT         NOT NULL,
     FOREIGN KEY (ClassId) REFERENCES Class (ClassID)
 );
+insert into student
+values(1,'Nguyen V','Thanh Hoa','09765643222',1,1),
+(2,'Nguyen L','Thanh Hoa','09765643422',0,2),
+(3,'Nguyen c','Ha Noi','097652333222',1,1);
 CREATE TABLE Subjects
 (
     SubId   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -24,7 +31,10 @@ CREATE TABLE Subjects
     Credit  TINYINT     NOT NULL DEFAULT 1 CHECK ( Credit >= 1 ),
     Statuss  BIT                  DEFAULT 1
 );
-
+insert into subjects
+values(1,'Mon Van',1,1),
+(2,'Mon Toan',2,1),
+(3,'Mon Anh',2,1);
 CREATE TABLE Mark
 (
     MarkId    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -36,3 +46,17 @@ CREATE TABLE Mark
     FOREIGN KEY (SubId) REFERENCES Subjects (SubId),
     FOREIGN KEY (StudentId) REFERENCES Student (StudentId)
 );
+SELECT st.Address, COUNT(st.StudentId) AS 'Số lượng học viên'
+FROM Student st
+GROUP BY st.Address;
+
+SELECT S.StudentId, S.StudentName, AVG(Mark)
+FROM Student S
+         join Mark M on S.StudentId = M.StudentId
+GROUP BY S.StudentId, S.StudentName
+HAVING AVG(Mark) > 15;
+
+SELECT S.StudentId, S.StudentName, AVG(Mark)
+FROM Student S join Mark M on S.StudentId = M.StudentId
+GROUP BY S.StudentId, S.StudentName
+HAVING AVG(Mark) >= ALL (SELECT AVG(Mark) FROM Mark GROUP BY Mark.StudentId);
